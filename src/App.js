b4288a51f04f21e7fc728afcafabaa58794a98d1
Ash <ashkanmi1995@gmail.com>
2026-04-01 18:18:29 +0100

    feat add basic UI layout for Todo App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import React from 'react';
+
 function App() {
   return (
     <div>
       <h1>React Todo App</h1>
-      <p>My third portfolio project.</p>
+
+      {/* Input field for new todo */}
+      <input type="text" placeholder="Enter a todo" />
+
+      {/* Button to add new todo */}
+      <button>Add Todo</button>
+
+      {/* Unordered list for todos */}
+      <ul></ul>
     </div>
   );
 }
