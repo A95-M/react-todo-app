0c6b613a64b3faec30de6fbea34dea832be5be43
Ash <ashkanmi1995@gmail.com>
2026-04-01 19:32:04 +0100

    feat: add input state and handle onChange

--- src/App.js
+++ src/App.js
@@ -5,11 +5,19 @@ function App() {
   // State to store todos
   const [todos, setTodos] = useState([]);
 
+  // State to store input value
+  const [input, setInput] = useState('');
+
   return (
     <div>
       <h1>React Todo App</h1>
 
-      <input type="text" placeholder="Enter a todo" />
+      <input 
+        type="text" 
+        placeholder="Enter a todo"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
 
       <button>Add Todo</button>
 
